c9e235a3d688636601a0d634863a4d22661d0a27
itsmahnoor <mahnoorsarfraz23@gmail.com>
2024-11-03 17:11:31 +0500

    Package install

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Scans all JavaScript/JSX files in src for Tailwind classes
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
